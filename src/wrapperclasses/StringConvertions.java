1b675d56a81b4f2847cc55de1c628c8038a31fba
Selenium <Selenium@172.31.98.140>
2018-05-10 15:56:59 -0400

    commit

--- src/wrapperclasses/StringConvertions.java
+++ src/wrapperclasses/StringConvertions.java
@@ -1,5 +1,17 @@
 package wrapperclasses;
 
 public class StringConvertions {
+	public static void main(String[] args) {
+		String str = "-1234";
+		Integer count = Integer.valueOf(str);
+		int count2 = Integer.parseInt(str);
+
+		if (count > 0) {
+			System.out.println("it is moire then 0");
+
+		} else {
+			System.out.println("Not more than 0");
+		}
+	}
 
 }
